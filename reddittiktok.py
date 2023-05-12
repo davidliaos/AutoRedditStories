0d02a0f980a7c08436db9df05954899026edda3d
David <davidliao9310@gmail.com>
2023-05-12 01:39:19 -0400

    newest up to date

--- reddittiktok.py
+++ reddittiktok.py
@@ -1,44 +1,57 @@
 import praw
 import os
+from uploader import uploadVideo
 from texttospeech import createTTS, wcreateTTS
 from subtitles import createsrt, format_time, get_screen_size, subtitle_generator, add_subtitles
 from videocreation import createVideo, createVideoMov
 from textprocess import createPostTextFile, preprocessText
 
+
+
+SESSION_ID = "98c156e53abe8c7c5452afc89a05ca18"
+TITLE = "thoughts?"
+TAGS = ["aita", "askreddit", "redditstories","tifu","redditreadings"]
+USERS = []
+
+
 if not os.path.exists("srt"):
     os.makedirs("srt")
-    print("srt does not exist, creating.")
+    print("srt directory does not exist, creating.")
 if not os.path.exists("posts"):
     os.makedirs("posts")
-    print("post does not exist, creating.")
+    print("post directory does not exist, creating.")
 
 if not os.path.exists("mp3"):
     os.makedirs("mp3")
-    print("mp3 does not exist, creating.")
+    print("mp3 directory does not exist, creating.")
 
 if not os.path.exists("mp4"):
     os.makedirs("mp4")
-    print("mp4 does not exist, creating.")
+    print("mp4 directory does not exist, creating.")
 
 if not os.path.exists("wav"):
     os.makedirs("wav")
-    print("wav does not exist, creating.")
+    print("wav directory does not exist, creating.")
 
 if not os.path.exists("results"):
     os.makedirs("results")
+    print("results directory does not exist, creating.")
 # Allows user to just straight up delete the folders when done without the hassle of deleting each file.
 # Set up Reddit API credentials
+
 reddit = praw.Reddit(
-    client_id="wsEcf3im17zm6vibQBkOfg",
-    client_secret="D3Bpg1bt6z-H8NmqlWOby3OpWiRxUw",
-    password="%z9rVMhC_qDtH.P",
-    user_agent="Reddit Bot V1.0",
-    username="dliaos",
+    client_id=os.environ["REDDIT_CLIENT_ID"],
+    client_secret=os.environ["REDDIT_CLIENT_SECRET"],
+    password=os.environ["REDDIT_PASSWORD"],
+    user_agent=os.environ["REDDIT_USER_AGENT"],
+    username=os.environ["REDDIT_USERNAME"],
 )
 
-# Define subreddit to search and get top posts from past day
-subreddit_name = "AmITheAsshole"
-posts = reddit.subreddit(subreddit_name).top(time_filter="day", limit=5)
+subreddit_name = os.environ["SUBREDDIT_NAME"]
+post_limit = int(os.environ["POST_LIMIT"])
+time_filter = os.environ["TIME_FILTER"]
+
+posts = reddit.subreddit(subreddit_name).top(time_filter=time_filter, limit=post_limit)
 
 # Define functions
 
@@ -67,15 +80,18 @@ for post in posts:
     
     # Check if post_id already exists
     if not checkPostId(post_id):
+        file = 'results/' + 'subtitled' + f'{post_id}.mp4'
         # Call function to create text file for the post
             # Print the post link
         print("Post link:", post_url)
         createPostTextFile(title, body, author, post_id,input_text)
+
         preprocessText(post_id)
         wcreateTTS(post_id,input_text)
-        createVideo(post_id,input_text)
+        createVideo(post_id)
         #createVideoMov(post_id)
         createsrt("mp4",post_id)
         add_subtitles(post_id)
+        uploadVideo(SESSION_ID, file, TITLE, TAGS, USERS)
         addPostId(post_id)
         print("----------------------------Post created successfully----------------------------")
