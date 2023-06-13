3e2684ab0cf4cb63c39cb0f7f1610b19c5eececc
David <davidliao9310@gmail.com>
2023-06-13 19:30:25 -0400

    adding heart emote to preprocess

--- textprocess.py
+++ textprocess.py
@@ -31,7 +31,8 @@ def preprocessText(post_id):
     #removes links 
     file_contents = re.sub(r'http\S+', '', file_contents) # Remove any links starting with http
     file_contents = re.sub(r'www\.\S+', '', file_contents) # Remove any links starting with www
-    
+    #removes silly unicode emojis
+    file_contents = file_contents.replace('♥️', ' ')
     # Write the updated contents back to the file
     with open(file_path, 'w', encoding='utf-8') as f:
         f.write(file_contents)
